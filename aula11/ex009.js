c8a8fe71968bb9dc779a31e85db83d93cb72a732
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-07-29 22:27:33 -0300

    Aula 11 Condições - Parte 1
    Condição If. Simples e Composta

---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if ( pais == 'Brasil'){
+    console.log('Brasileiro!')
+}else{
+    console.log('Estrangeira!')
+}
+
+// Utilizando a negação
+
+if ( pais != 'Brasil'){
+    console.log('Estrangeira!')
+}else{
+    console.log('Brasileiro!')
+}
