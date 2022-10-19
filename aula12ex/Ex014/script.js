6718219a31c8ec24edc3ba5602ee6e4213d9ad5e
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-10-18 22:11:31 -0300

    ex014 - Hora do dia

--- aula12ex/Ex014/script.js
+++ aula12ex/Ex014/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    if (hora >= 0 && hora <12) {
+        msg.innerHTML = `Agora são ${hora} horas.<br>Bom dia!`
+        img.src = 'img/manha.png'
+        document.body.style.background = '#FECE74'
+    } else if (hora >= 12 && hora < 18){
+        msg.innerHTML = `Agora são ${hora} horas.<br>Boa Tarde!`
+        img.src = 'img/tarde.png'
+        document.body.style.background = '#F28705'
+    } else {
+        msg.innerHTML = `Agora são ${hora} horas.<br>Boa noite!`
+        img.src = 'img/noite.png'
+        document.body.style.background = '#252526'
+    }
+}
