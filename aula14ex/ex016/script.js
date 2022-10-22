2616e3fb5be871d11c0c8a7c31c9f3c496c14375
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-10-21 23:02:02 -0300

    ex016 começo javascript
    Iniciado laço while

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,12 @@
-alert('Olá!')
+function contar() {
+    var inicio = window.document.querySelector('input#inicio')
+    var fim = document.querySelector('input#fim')
+    var passo = document.getElementById('passo')
+    var res = document.querySelector('div#res')
+
+    while (inicio <= fim) {
+        
+    }
+
+    res.innerHTML = `Começa em ${inicio.value}, terminal em ${fim.value}. Indo de ${passo.value} em ${passo.value}.`
+}
