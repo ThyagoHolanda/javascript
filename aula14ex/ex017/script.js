02b504922a2d6193be72ba01142d3abe02811a0c
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-10-23 11:07:00 -0300

    ex017 tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,13 @@
-alert('Olá!')
+function gerar() {
+    var numero = Number(document.querySelector('input#numero').value)
+    var res = document.querySelector('select#tabuada')
+
+    for (var contar = 1; contar <= 10; contar++) {
+        var num2 = numero * contar
+        if (contar == 1) {
+            res.innerHTML = `<option value="numer${contar}">${numero} x ${contar} = ${num2}</option>`        
+        }else {
+            res.innerHTML += `<option value="numer${contar}">${numero} x ${contar} = ${num2}</option>` 
+        }
+    }
+}
