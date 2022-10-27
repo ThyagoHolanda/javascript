cf168e6008498dd1d453fd8d09bd9c698e4afe5c
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-10-27 19:15:40 -0300

    Update Script
    Outra forma de executar o Script

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,19 +1,31 @@
 function gerar() {
-    var numero = document.querySelector('input#numero')
-    var res = document.querySelector('select#tabuada')
-    var num = Number(numero.value)
+    let numero = document.querySelector('input#numero')
+    let res = document.querySelector('select#tabuada')
+    let num = Number(numero.value)
 
     if (numero.value.length == 0) {
         alert("Digite um numero!")
     }else {
-        for (var i = 1; i <= 10; i++) {
-            var numres = num * i
+        res.innerHTML = ""
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${i} = ${num*i}`
+            item.value = `tab${i}`
+            res.appendChild(item)
+            }
+        }
+    
+    /*if (numero.value.length == 0) {
+        alert("Digite um numero!")
+    }else {
+        for (let i = 1; i <= 10; i++) {
+            let numres = num * i
             if (i == 1) {
                 res.innerHTML = `<option value="numer${i}">${num} x ${i} = ${numres}</option>`        
             }else {
                 res.innerHTML += `<option value="numer${i}">${num} x ${i} = ${numres}</option>` 
             }
         }
-    }
+    }*/
 
 }
