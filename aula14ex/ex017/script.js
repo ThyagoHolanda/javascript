113fec228e73946cab901da350dbc3ed58c8ffd7
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-10-27 18:59:13 -0300

    Update script.js
    Alerta de erro. "Digitar um numero"

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,13 +1,19 @@
 function gerar() {
-    var numero = Number(document.querySelector('input#numero').value)
+    var numero = document.querySelector('input#numero')
     var res = document.querySelector('select#tabuada')
+    var num = Number(numero.value)
 
-    for (var contar = 1; contar <= 10; contar++) {
-        var num2 = numero * contar
-        if (contar == 1) {
-            res.innerHTML = `<option value="numer${contar}">${numero} x ${contar} = ${num2}</option>`        
-        }else {
-            res.innerHTML += `<option value="numer${contar}">${numero} x ${contar} = ${num2}</option>` 
+    if (numero.value.length == 0) {
+        alert("Digite um numero!")
+    }else {
+        for (var i = 1; i <= 10; i++) {
+            var numres = num * i
+            if (i == 1) {
+                res.innerHTML = `<option value="numer${i}">${num} x ${i} = ${numres}</option>`        
+            }else {
+                res.innerHTML += `<option value="numer${i}">${num} x ${i} = ${numres}</option>` 
+            }
         }
     }
+
 }
