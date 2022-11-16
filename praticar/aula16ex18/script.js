1bb8e7861aa3031586f911f862c2e888e53f7e42
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2022-11-15 23:05:34 -0300

    Update script.js
    incluso código do Professor Guanabara

--- praticar/aula16ex18/script.js
+++ praticar/aula16ex18/script.js
@@ -6,7 +6,7 @@ let valores = []
 
 
 function isNumero(n) {
-    if (n >=1 && n <=100) {
+    if (Number(n) >=1 && Number(n) <=100) {
         //alert("isNumero true")
         return true
     }else {
@@ -16,7 +16,7 @@ function isNumero(n) {
 }
 
 function inList(n,l){
-    if (l.indexOf(n) == -1) {
+    if (l.indexOf(Number(n)) == -1) {
         //alert("inList true")
         return true
     }else {
@@ -26,52 +26,74 @@ function inList(n,l){
 }
 
 function adicionar() {
-    if (isNumero(num.value) && inList(num.value,valores)) {
+    if (isNumero(Number(num.value)) && inList(Number(num.value),valores)) {
         //alert(`O numero ${num.value} está entre 1 e 100 e não esta na lista`)
-        valores.push(num.value)
+        valores.push(Number(num.value))
 
         let item = document.createElement('option')
-        item.text = `Valor ${num.value} foi adicionado`
+        item.text = `Valor ${Number(num.value)} foi adicionado`
         lista.appendChild(item)
         res.innerHTML = ""
     }else{
         alert("Valor invalido ou não esta na lista!")
     }
-    
+    //num.value = ""
+    num.focus()
 }
 
 function finalizar() {
-    let maior = Math.max(...valores)
-    /*
-    Outra forma de fazer o MAIOR
-    valores.reduce(function(a,b) {
-        return Math.MAX(a,b)
-    })
-    */
+    if (valores.length == 0) {
+        alert("Nenhum valor adicionado. Favor adicionar um numero!")
+        num.focus()
+    }else {
+        let maior = Math.max(...valores)
+        /*
+        Outra forma de fazer o MAIOR
+        valores.reduce(function(a,b) {
+            return Math.MAX(a,b)
+        })
+        */
 
-    let menor = Math.min(...valores)
-    /*
-    Outra forma de fazer o MENOR
-    valores.reduce(function(a,b) {
-        return Math.min(a,b)
-    })
-    */
+        let menor = Math.min(...valores)
+        /*
+        Outra forma de fazer o MENOR
+        valores.reduce(function(a,b) {
+            return Math.min(a,b)
+        })
+        */
 
-    let soma = valores.map(Number).reduce(function(a, b) {
-        return a + b
-    }) 
-    /*
-    Soma de todos os elementos do array
-    .map() Indica que o array é tipo Number
-    */
+        let soma = valores.reduce(function(a, b) {
+            return a + b
+        }) 
+        /*
+        Soma de todos os elementos do array
+        .map() Indica que o array é tipo Number
+        */
 
-    let media = soma/valores.length
+        /*
+        Outra forma de fazer o MAIOR MENOR SOMA e MEDIA
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if ( valores[pos] < menor)
+                menor = valores[pos]
+        }
+        */
 
-    res.innerHTML = `<br>Ao todo, temos ${valores.length} numeros cadastrados.<br>`
-    res.innerHTML += `<br>O maior valor informado foi ${maior}.<br>`
-    res.innerHTML += `<br>O menor valor informado foi ${menor}.<br>`
-    res.innerHTML += `<br>Somando todos os valores, temos ${soma}.<br>`
-    res.innerHTML += `<br>A média dos valores digitados ${media}.<br>`
+        let media = soma/valores.length
+
+        res.innerHTML = `<p>Ao todo, temos ${valores.length} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados ${media}.</p>`
+    }
+    
 }
 
 
